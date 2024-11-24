6852a0862faf424aee39972a433b251227f5bbd7
BetterCallSaul22 <Alfonso.Zuluaga8@gmail.com>
2024-11-23 22:04:33 -0600

    Añadiendo los cambios hechos a la lógica...

--- app.js
+++ app.js
@@ -108,86 +108,82 @@ const obtenerPokeRival = () =>{
 //Mostrar el ganador
 // Comentario de prueba
 const combate = ()=>{
-<<<<<<< HEAD
-    
-=======
     console.log("Están peleando lmao")
->>>>>>> origin/Percival
 }
 
 //Cuando no hay efectividad de ataque el valor es 1, ya que como se multiplica lo mantiene igual
 //Es un diccionario clave:valor
 const tablaEfectividad = {
     steel: {
-        steel: 0.5, water: 0.5, bug: 1, dragon: 0.5, electric: 0.5, ghost: 1, fire: 0.5, ice: 2, fighting: 1, 
-        normal: 1, grass: 1, psychic: 1, rock: 2, dark: 1, ground: 1, poison: 0.5, flying: 1, fairy: 2
+        bug: 1, ghost: 1, ice: 2, fighting: 1, 
+        normal: 1, grass: 1, psychic: 1, rock: 2, dark: 1, ground: 1, flying: 1, fairy: 2
     },
     water: {
-        fire: 2, water: 0.5, grass: 0.5, ground: 2, rock: 2, dragon: 0.5, steel: 1, electric: 1, bug: 1, 
+        fire: 2, ground: 2, rock: 2, steel: 1, electric: 1, bug: 1, 
         ghost: 1, fighting: 1, normal: 1, psychic: 1, dark: 1, poison: 1, flying: 1, ice: 1, fairy: 1
     },
     bug: {
-        grass: 2, psychic: 2, dark: 2, fire: 0.5, fighting: 0.5, poison: 0.5, flying: 0.5, steel: 0.5,
-        water: 1, bug: 1, dragon: 1, electric: 1, ghost: 0.5, ice: 1, normal: 1, rock: 1, ground: 1, fairy: 0.5
+        grass: 2, psychic: 2, dark: 2,
+        water: 1, bug: 1, dragon: 1, electric: 1, ghost: 0.5, ice: 1, normal: 1, rock: 1, ground: 1,
     },
     dragon: {
-        dragon: 2, steel: 0.5, fairy: 0, fire: 1, water: 1, grass: 1, electric: 1, bug: 1, ghost: 1, 
+        dragon: 2, fire: 1, water: 1, grass: 1, electric: 1, bug: 1, ghost: 1, 
         fighting: 1, normal: 1, psychic: 1, rock: 1, dark: 1, poison: 1, flying: 1, ice: 1, ground: 1
     },
     electric: {
-        water: 2, flying: 2, ground: 0, grass: 0.5, electric: 0.5, steel: 1, dragon: 0.5, bug: 1, ghost: 1,
+        water: 2, flying: 2, steel: 1,  bug: 1, ghost: 1,
         fire: 1, fighting: 1, ice: 1, normal: 1, psychic: 1, rock: 1, dark: 1, poison: 1, fairy: 1
     },
     ghost: {
-        psychic: 2, ghost: 2, normal: 0, dark: 0.5, steel: 1, water: 1, bug: 1, dragon: 1, electric: 1,
+        psychic: 2, ghost: 2, steel: 1, water: 1, bug: 1, dragon: 1, electric: 1,
         fire: 1, ice: 1, fighting: 1, grass: 1, rock: 1, ground: 1, poison: 1, flying: 1, fairy: 1
     },
     fire: {
-        grass: 2, ice: 2, bug: 2, steel: 2, water: 0.5, rock: 0.5, fire: 0.5, dragon: 0.5, ground: 1,
+        grass: 2, ice: 2, bug: 2, steel: 2, ground: 1,
         electric: 1, ghost: 1, fighting: 1, normal: 1, psychic: 1, dark: 1, poison: 1, flying: 1, fairy: 1
     },
     fairy: {
-        dragon: 2, fighting: 2, dark: 2, fire: 0.5, poison: 0.5, steel: 0.5, water: 1, bug: 1, electric: 1,
+        dragon: 2, fighting: 2, dark: 2, water: 1, bug: 1, electric: 1,
         ghost: 1, ice: 1, normal: 1, grass: 1, psychic: 1, rock: 1, ground: 1, flying: 1, fairy: 1
     },
     ice: {
-        grass: 2, ground: 2, flying: 2, dragon: 2, fire: 0.5, water: 0.5, ice: 0.5, steel: 0.5, bug: 1, 
+        grass: 2, ground: 2, flying: 2, dragon: 2, bug: 1, 
         electric: 1, ghost: 1, fighting: 1, normal: 1, psychic: 1, rock: 1, dark: 1, poison: 1, fairy: 1
     },
     fighting: {
-        normal: 2, ice: 2, rock: 2, dark: 2, steel: 2, poison: 0.5, flying: 0.5, psychic: 0.5, bug: 0.5,
+        normal: 2, ice: 2, rock: 2, dark: 2, steel: 2,
         fairy: 0.5, ghost: 0, water: 1, dragon: 1, electric: 1, fire: 1, fighting: 1, grass: 1, ground: 1
     },
     normal: {
-        rock: 0.5, ghost: 0, steel: 0.5, water: 1, bug: 1, dragon: 1, electric: 1, fire: 1, ice: 1,
+        water: 1, bug: 1, dragon: 1, electric: 1, fire: 1, ice: 1,
         fighting: 1, grass: 1, psychic: 1, dark: 1, ground: 1, poison: 1, flying: 1, fairy: 1
     },
     grass: {
-        water: 2, ground: 2, rock: 2, fire: 0.5, grass: 0.5, poison: 0.5, flying: 0.5, bug: 0.5, dragon: 0.5,
-        steel: 0.5, electric: 1, ghost: 1, ice: 1, fighting: 1, normal: 1, psychic: 1, dark: 1, fairy: 1
+        water: 2, ground: 2, rock: 2,
+        electric: 1, ghost: 1, ice: 1, fighting: 1, normal: 1, psychic: 1, dark: 1, fairy: 1
     },
     psychic: {
-        fighting: 2, poison: 2, psychic: 0.5, steel: 0.5, dark: 0, water: 1, bug: 1, dragon: 1, electric: 1,
+        fighting: 2, poison: 2, water: 1, bug: 1, dragon: 1, electric: 1,
         ghost: 1, fire: 1, ice: 1, normal: 1, grass: 1, rock: 1, ground: 1, flying: 1, fairy: 1
     },
     rock: {
-        fire: 2, ice: 2, flying: 2, bug: 2, fighting: 0.5, ground: 0.5, steel: 0.5, water: 1, dragon: 1,
+        fire: 2, ice: 2, flying: 2, bug: 2, water: 1, dragon: 1,
         electric: 1, ghost: 1, normal: 1, grass: 1, psychic: 1, rock: 1, dark: 1, poison: 1, fairy: 1
     },
     dark: {
-        psychic: 2, ghost: 2, fighting: 0.5, dark: 0.5, fairy: 0.5, steel: 1, water: 1, bug: 1, dragon: 1,
+        psychic: 2, ghost: 2, steel: 1, water: 1, bug: 1, dragon: 1,
         electric: 1, fire: 1, ice: 1, normal: 1, grass: 1, rock: 1, ground: 1, poison: 1, flying: 1
     },
     ground: {
-        fire: 2, electric: 2, poison: 2, rock: 2, steel: 2, grass: 0.5, bug: 0.5, flying: 0, water: 1,
+        fire: 2, electric: 2, poison: 2, rock: 2, steel: 2, water: 1,
         dragon: 1, ghost: 1, ice: 1, fighting: 1, normal: 1, psychic: 1, dark: 1, fairy: 1
     },
     poison: {
-        grass: 2, fairy: 2, poison: 0.5, ground: 0.5, rock: 0.5, ghost: 0.5, steel: 0, water: 1, bug: 1,
+        grass: 2, fairy: 2, water: 1, bug: 1,
         dragon: 1, electric: 1, fire: 1, ice: 1, fighting: 1, normal: 1, psychic: 1, flying: 1, dark: 1
     },
     flying: {
-        grass: 2, fighting: 2, bug: 2, electric: 0.5, rock: 0.5, steel: 0.5, water: 1, dragon: 1, ghost: 1,
+        grass: 2, fighting: 2, bug: 2, water: 1, dragon: 1, ghost: 1,
         fire: 1, ice: 1, normal: 1, psychic: 1, ground: 1, poison: 1, dark: 1, fairy: 1
     }
 };
